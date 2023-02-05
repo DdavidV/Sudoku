20f8fed39e08a8bf016218bf5aa029130fa0ab59
DdavidV <themani@hotmail.hu>
2023-02-05 23:26:35 +0100

    Sudoku solver

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    private static final int SIZE = 9;
+    public static void main(String[] args) {
+        int[][] board = {
+                {3, 0, 6, 5, 0, 8, 4, 0, 0},
+                {5, 2, 0, 0, 0, 0, 0, 0, 0},
+                {0, 8, 7, 0, 0, 0, 0, 3, 1},
+                {0, 0, 3, 0, 1, 0, 0, 8, 0},
+                {9, 0, 0, 8, 6, 3, 0, 0, 5},
+                {0, 5, 0, 0, 9, 0, 6, 0, 0},
+                {1, 3, 0, 0, 0, 0, 2, 5, 0},
+                {0, 0, 0, 0, 0, 0, 0, 7, 4},
+                {0, 0, 5, 2, 0, 6, 3, 0, 0}
+        };
+
+        Solver solver = new Solver(board);
+        solver.solve();
+    }
+}
---
+++ src/Solver.java
@@ -0,0 +1,100 @@
+public class Solver {
+    private int[][] board;
+    private static final int SIZE = 9;
+
+    Solver(int[][] board){
+        this.board = board;
+    }
+
+    private void printBoard(int[][] board) {
+        for(int row = 0; row < SIZE; row++){
+            if(row % 3 == 0 && row != 0){
+                System.out.println("---+---+---");
+            }
+            for(int column = 0; column < SIZE; column++){
+                if(column % 3 == 0 && column !=0){
+                    System.out.print("|");
+                }
+                System.out.print(board[row][column]);
+            }
+            System.out.println();
+        }
+    }
+
+    private boolean numberInRow(int[][] board, int number, int row){
+        //Compare a number with all the numbers in a row
+        for(int i = 0; i < SIZE; i++){
+            if(board[row][i] == number){
+                return true;
+            }
+        }
+        return false;
+    };
+
+    private boolean numberInColumn(int[][] board, int number, int column){
+        //Compare a number with all the number in a column
+        for(int i = 0; i < SIZE; i++){
+            if(board[i][column] == number){
+                return true;
+            }
+        }
+        return false;
+    };
+
+    private boolean numberInBox(int[][] board, int number, int row, int column){
+        //Get top left corner of a box (3x3 space)
+        //Every box row and column can be: 1, 2, 3
+        //Compare a number if it appears in a box
+        int boxRow = row - row % 3;
+        int boxColumn = column - column % 3;
+
+        for (int i = boxRow; i < boxRow + 3; i++){
+            for (int j = boxColumn; j < boxColumn + 3; j++){
+                if(board[i][j] == number){
+                    return true;
+                }
+            }
+        }
+        return false;
+    };
+
+    private boolean validNumber(int[][] board, int number, int row, int column){
+        //We need to negate the methods because they will return false if a number is valid by that rule
+        return !numberInRow(board, number, row) &&
+                !numberInColumn(board, number, column) &&
+                !numberInBox(board, number, row, column);
+    }
+
+    private boolean solver(int[][] board){
+        //Backtracking algorithm
+        for (int row = 0; row < SIZE; row++){
+            for(int column = 0; column < SIZE; column++){
+                if(board[row][column] == 0){
+                    for(int numberToTry = 1; numberToTry <= SIZE; numberToTry++){
+                        if(validNumber(board,numberToTry, row, column)){
+                            board[row][column] = numberToTry;
+
+                            if(solver(board)){
+                                return true;
+                            } else {
+                                board[row][column] = 0;
+                            }
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public void solve(){
+        if(solver(board)){
+            printBoard(board);
+            System.out.println("Solved");
+        } else {
+            System.out.println("Unsolvable");
+        }
+    }
+
+}
